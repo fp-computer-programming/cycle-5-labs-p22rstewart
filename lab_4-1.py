6d383833061c44d43e27f07a0aa10b9d53709b6a
Ryan Stewart <P22rstewart@fairfieldprep.org>
2021-11-02 20:54:25 -0400

    HW 4

---
+++ lab_4-1.py
@@ -0,0 +1,11 @@
+# Author: RTS 11/2/21
+print("Fairfield " + "Prep")
+
+print("-" * 20)
+
+print("I\'m loving this short story I\'m reading, \"The Fall of the House of Usher\"")
+print(len("I\'m loving this short story I\'m reading, \"The Fall of the House of Usher\""))
+string = "apple"
+l = string[3]
+print(l)
+
